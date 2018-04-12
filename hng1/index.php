9262e8c310cd65f93448afb80f5543e6862b6ca6
Adeboye Sarah <talk2oluwasarah@gmail.com>
2018-04-12 18:21:31 +0100

    Initial commit

---
+++ hng1/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Delectable Sarah</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Snippet|Averia+Sans+Libre" rel="stylesheet">
+  <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+</head>
+
+<body>
+  <?php
+    date_default_timezone_set('Africa/Lagos');
+  ?>
+  <div class="container">
+    <header><i class=" icon-clock far fa-fw fa-clock"></i><span class="date"><?php echo date("D M d, Y"); ?></span> | <span class="time"><?php echo date("g:i a"); ?></span></header>
+    <section class="title">
+      <h1 class="name">Sarah Temitope</h1>
+      <p class="labels">Web Developer ✿ Lover of Life</p>
+    </section>
+    <section class="socials">
+      <a href="https://www.instagram.com/elegant_garlie0" class="fa-icon" target="_blank">
+        <i class="fab fa-fw fa-instagram"></i>
+      </a>
+      <a href="https://www.twitter.com/sarahtemy" class="fa-icon" target="_blank">
+        <i class="fab fa-fw fa-twitter"></i>
+      </a>
+      <a href="https://www.facebook.com/temipet" class="fa-icon" target="_blank">
+        <i class="fab fa-fw fa-facebook-f"></i>
+      </a>
+      <a href="https://www.github.com/temipet" class="fa-icon" target="_blank">
+        <i class="fab fa-fw fa-github-alt"></i>
+      </a>
+      <a href="https://ng.linkedin.com/in/adeboye-sarah-956a40143" class="fa-icon" target="_blank">
+        <i class="fab fa-fw fa-linkedin-in"></i>
+      </a>
+      <a href="mailto:talk2oluwasarah@gmail.com" class="fa-icon" target="_blank">
+        <i class="far fa-fw fa-envelope"></i>
+      </a>
+    </section>
+  </div>
+</body>
+
+</html>
